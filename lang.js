a1597aa7194afade481baecba127fff36ac02054
rainbow-pill-collective <rainbow.pill@posteo.com>
2025-07-29 17:51:49 +0200

    Update lang.js

--- lang.js
+++ lang.js
@@ -1,14 +1,14 @@
 function setLanguage(lang) {
-    fetch('lang/' + lang + '.json')
-        .then(response => response.json())
-        .then(translations => {
-            document.querySelectorAll('[data-i18n]').forEach(el => {
-                const key = el.getAttribute('data-i18n');
-                if (translations[key]) {
-                    el.textContent = translations[key];
-                }
-            });
-        });
+  fetch('lang/' + lang + '.json')
+    .then(response => response.json())
+    .then(translations => {
+      document.querySelectorAll('[data-i18n]').forEach(el => {
+        const key = el.getAttribute('data-i18n');
+        if (translations[key]) {
+          el.textContent = translations[key];
+        }
+      });
+    });
 }
 
 const urlParams = new URLSearchParams(window.location.search);
